7bd316db5064a018527bbebc18f1bbe68154e459
Grisha Soloviev <gsoloviev@htc-cs.ru>
2019-07-25 15:14:17 +0400

    second commit

--- TestFramework/TestFramework.h
+++ TestFramework/TestFramework.h
@@ -14,6 +14,9 @@ FOUNDATION_EXPORT double TestFrameworkVersionNumber;
 //! Project version string for TestFramework.
 FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
 
+
+
+
 // In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
 
 
